2c18bb867dc36f8d9c9dfb4e88f2faf1e3144f83
Benjamin Lellouch <blellouch@gmail.com>
2022-04-11 13:25:52 +0100

    Kernel and PC filters are now generated from the double integral from the paper

--- Layers.py
+++ Layers.py
@@ -1,14 +1,14 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from klein import klein_filter, generate_kernel
+from klein import generate_klein_filter, generate_pc_filter
 
 class KF_Layer(nn.Module):
 
    def __init__(self, size: int, slices: int):
        # Need to create sqrt(slices) evenly spaced angles for both thetas
-       thetas1 = [np.pi/2, np.pi/4, 0, -np.pi/4]
-       thetas2 = [0, np.pi/2, np.pi, -np.pi/2]
-
+       angle = sqrt(slices)
+       thetas1 =  [(i*np.pi)/angle for i in range(angle)]
+       thetas2 = [(i*np.pi*2)/angle for i in range(angle)]
        self.filters = []
 
        for theta2 in thetas2:
@@ -16,19 +16,18 @@ class KF_Layer(nn.Module):
                filters.append(generate_kernel(size, theta1, theta2))
 
 
-  def forward(self, x):
-      output = []
-      for filter in self.filters:
-          output.append(F.conv2d(x, filter).squeeze(0))
-      return torch.tensor(output)
+   def forward(self, x):
+       output = []
+       for filter in self.filters:
+           output.append(F.conv2d(x, filter).squeeze(0))
+       return torch.tensor(output)
 
 
-
- class CF_Layer(nn.Module):
+class CF_Layer(nn.Module):
 
    def __init__(self, size: int, slices: int):
        # Need to generate slices number of evenly spaced angles around a circle
-       thetas = [np.pi/2, np.pi/4, 0, -np.pi/4]
+       thetas = [(i*np.pi)/size for i in range(size)]
 
        self.filters = []
 
@@ -36,7 +35,7 @@ class KF_Layer(nn.Module):
            filters.append(generate_kernel(size, theta))
 
 
-  def forward(self, x):
+   def forward(self, x):
       output = []
       for filter in self.filters:
           output.append(F.conv2d(x, filter).squeeze(0))
--- klein.py
+++ klein.py
@@ -3,35 +3,54 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.cm as cm
 import os
+from scipy.integrate import quad,dblquad
 
 def Q(t: float) -> float:
-    return 2*t**2 - 1
+    return (2*t)**2 - 1
 
 # Calculates Klein Filter value for position (x,y)
-def klein_filter(theta1: float, theta2: float, x: int, y: int) -> float:
+def klein(y: int, x: int, theta1: float, theta2: float) -> float:
     return np.sin(theta2)*(np.cos(theta1)*x+ np.sin(theta1)*y) + np.cos(theta2)*Q(np.cos(theta1)*x + np.sin(theta1)*y)
 
 # Generates a Circle Filter value for position (x,y)
-def primary_circle(theta: float, x: int, y: int) -> float:
-    return np.cos(theta)*x + np.sin(theta)*y
+def primary_circle(y: int, x: int,theta: float) -> float:
+    return klein(theta, np.pi/2, x, y)
 
-# Generates a complete Klein or Primary Circle filter/kernel
-def generate_kernel(size: int, theta1: float, theta2 = np.pi /2, circle = False) -> np.ndarray:
-    results = []
-    # This linspace seems to work for KFs but not CFs
-    # We might need to find a different way of choosing our x,y values
-    linspace = np.linspace(-1,1,size)
-    for x in linspace :
-        row = []
-        for y in linspace :
-            kf = klein_filter(theta1, theta2, x, y) if not circle else primary_circle(theta1, x, y)
-            row.append(kf)
-        results.append(row)
-    results = np.array(results)
-    return results 
+#generates klein of specific size and angle
+def generate_klein_filter(size: int, theta1: float, theta2: float) -> np.ndarray:
+
+    klein_filter = np.zeros([size,size])
+    for x in range(size):
+        for y in range(size):
+           xmin = -1.0 + ((2*x) / (2*size))
+           xmax = -1.0 + ((2*(x+1.0)) / (2*size))
+           ymin = -1.0 + ((2*y) / (2*size))
+           ymax = -1.0 + ((2*(y+1.0)) /(2*size))
+
+
+           Filter = dblquad(klein,xmin,xmax,ymin,ymax, args=(theta1,theta2))[0]
+           klein_filter[y][x] = Filter
+    return np.array(klein_filter)
+
+#generates primary circle filter with specific size and angle
+def generate_pc_filter(size: int, theta1: float) -> np.ndarray:
+
+    pc_filter = np.zeros([size,size])
+    for x in range(size):
+        for y in range(size):
+           xmin = -1.0 + ((2*x) / (2*size))
+           xmax = -1.0 + ((2*(x+1.0)) / (2*size))
+           ymin = -1.0 + ((2*y) / (2*size))
+           ymax = -1.0 + ((2*(y+1.0)) /(2*size))
+
+
+           Filter = dblquad(primary_circle,xmin,xmax,ymin,ymax, args=(theta1))[0]
+           klein_filter[y][x] = Filter
+    return np.array(klein_filter)
+
 
 # Displays all combinations
-def display_kernels(thetas: [float], size: int, circle = False) -> None:
+def display_kernels(thetas: [float], size: int, thetas2 = None, circle = False) -> None:
 
     try:
         os.mkdir('fig')
@@ -40,15 +59,15 @@ def display_kernels(thetas: [float], size: int, circle = False) -> None:
 
     if not circle:
         for theta1 in thetas:
-            for theta2 in thetas:
-                kfilter = generate_kernel(size, theta1, theta2)
+            for theta2 in thetas2:
+                kfilter = generate_klein_filter(size, theta1, theta2)
                 ax = sns.heatmap(kfilter, cmap=cm.gray)
                 ax.set_title("Theta1: {}\u03c0 and Theta2: {}\u03c0".format(theta1 / np.pi, theta2 / np.pi))
                 plt.savefig('fig/th1-{}\u03c0_th2-{}\u03c0.pdf'.format(theta1 / np.pi, theta2/ np.pi))
                 plt.show()
     else:
         for theta1 in thetas:
-                kfilter = generate_kernel(size, theta1)
+                kfilter = generate_pc_filter(size, theta1)
                 ax = sns.heatmap(kfilter, cmap=cm.gray)
                 ax.set_title("Theta1: {}\u03c0 and Theta2: {}\u03c0".format(theta1 / np.pi, 0.5))
                 plt.savefig('fig/th1-{}\u03c0_th2-{}\u03c0.pdf'.format(theta1 / np.pi, 0,5))
