5506f187333bcd90de7b78959ac5c64576e5dbbf
Gabija Petroskeviciute <gabija@Gabijas-MacBook-Pro.local>
2022-04-15 11:30:21 +0100

    Load CIRAR-10 cats and dogs

--- Experiments/Generalizability/data.py
+++ Experiments/Generalizability/data.py
@@ -1,5 +1,6 @@
 from torchvision import transforms, datasets
-from torch.utils.data import ConcatDataset
+from torch.utils.data import ConcatDataset, Subset
+import torch
 
 def load_MINST(download=False):
     trainset = datasets.MNIST(".", train=True, download=download,
@@ -20,3 +21,29 @@ def load_SVHN(download=False):
     testset = datasets.SVHN('SVHN/', split='test', download=download,
                             transform=svhn_transform)
     return ConcatDataset([trainset, testset])  # 73257 + 26032 = 99289 samples
+
+
+def load_CIFAR_cats_dogs(download=False):
+    cifar_transform = transforms.Compose([
+        transforms.Grayscale(),
+        transforms.ToTensor()
+    ]) # image from [3, 32, 32] to [1, 32, 32]
+    trainset = datasets.CIFAR10(root='CIFAR/', train=True, download=True,
+                                transform=cifar_transform)
+    testset = datasets.CIFAR10(root='CIFAR/', train=False, download=True,
+                               transform=cifar_transform)
+
+    # Create a subset of two classes - cats and dogs
+    # Use masking to pick samples from the datasets
+    train_mask = (torch.tensor(trainset.targets) == trainset.classes.index('cat')) | (
+                torch.tensor(trainset.targets) == trainset.classes.index('dog'))
+    train_indices = train_mask.nonzero().reshape(-1)
+    test_mask = (torch.tensor(testset.targets) == testset.classes.index('cat')) | (
+                torch.tensor(testset.targets) == testset.classes.index('dog'))
+    test_indices = test_mask.nonzero().reshape(-1)
+
+    train_subset = Subset(trainset, train_indices)
+    test_subset = Subset(testset, test_indices)
+
+    return ConcatDataset([train_subset, test_subset])  # 10000 + 2000 = 12000
+
